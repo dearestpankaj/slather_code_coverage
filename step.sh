a45e3a932326d239e654083e25ec70cf5c225c35
Pankaj Sachdeva <dearestpankaj@gmail.com>
2022-10-31 14:44:57 +0100

    Update step.sh

--- step.sh
+++ step.sh
@@ -3,4 +3,13 @@ set -ex
 
 gem install slather
 
-slather coverage --html -s --scheme 'Vivy Staging' --workspace Vivy.xcworkspace Vivy.xcodeproj
+slather coverage \
+  --workspace $workspace \
+  --scheme $scheme \
+  --output-directory $BITRISE_DEPLOY_DIR/$output_dir_path \
+  --source-directory $source_dir_path \
+  --binary-basename $tested_product_name \
+  --html \
+  $project
+
+envman add --key BITRISE_SLATHER_HTML_PATH --value "$BITRISE_DEPLOY_DIR/$output_dir_path/index.html"
